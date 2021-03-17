2c1e41252b742352ecfc4006df1b69081b44e541
JuniorEinstein <cordelia273@naver.com>
2021-03-17 16:17:20 +0900

    change permissions

--- alto/settings.py
+++ alto/settings.py
@@ -198,6 +198,9 @@ JWT_AUTH = {
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
+    'http://127.0.0.1',
+    'http://127.0.0.1:3000',
+    'http://localhost',
     'http://localhost:3000',
     'http://158.247.197.250',
     'http://158.247.197.250:3000',
