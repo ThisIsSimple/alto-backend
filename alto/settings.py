2e637b952c4fb19e193e77b694001ff503983c00
JuniorEinstein <cordelia273@naver.com>
2021-03-17 16:03:17 +0900

    cors whielist updated

--- alto/settings.py
+++ alto/settings.py
@@ -195,8 +195,8 @@ JWT_AUTH = {
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-    'http://158.247.197.250',
-    'http://158.247.197.250:3000',
-]
+# CORS_ORIGIN_WHITELIST = [
+#     'http://localhost:3000',
+#     'http://158.247.197.250',
+#     'http://158.247.197.250:3000',
+# ]
